0c2f5339a384743e01bc7931665fa2029e796056
Slamova Meruyert <11d.meruyerts@gmail.com>
2024-09-22 18:44:47 +0500

    add websockets chat

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.pages.router import router as router_pages
 from fastapi.staticfiles import StaticFiles
 
+from src.chat.router import router as router_chat
+
 app = FastAPI(title="Trading App")
 
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
@@ -32,15 +34,19 @@ app.include_router(
 app.include_router(router_operation)
 app.include_router(router_tasks)
 app.include_router(router_pages)
+app.include_router(router_chat)
 
 
 origins = [
     "http://localhost:3000",
+        "http://localhost:8000",
+
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
