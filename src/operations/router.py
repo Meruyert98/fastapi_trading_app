d2367a93ced8109152bc5065a08e536eb20afe0e
Slamova Meruyert <11d.meruyerts@gmail.com>
2024-09-22 13:05:17 +0500

    redis caching

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,8 +1,10 @@
 
+import time
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from fastapi_cache.decorator import cache
 from src.database import get_async_session
 from src.operations.models import operation
 from src.operations.schemas import OperationCreate
@@ -11,6 +13,12 @@ router = APIRouter(
     prefix="/operations",
     tags=["Operation"]
 )
+
+@router.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return "Много много данных, которые вычислялись сто лет"
 
 @router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
